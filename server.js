8bab6edd4d48721b8d106e72db9c9cf52077eaa4
anacomc <46031201+anacomc@users.noreply.github.com>
2026-08-17 18:02:12 -0400

    Add timestamp to download log message

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get(['/gracias', '/gracias.html'], (req, res) => {
 
 // 3. PROXY DE DESCARGA BINARIA (OCULTA EL ARCHIVO REAL DEL DISCO)
 app.get('/ejecutar-descarga-segura', (req, res) => {
-    console.log("🚀 DESCARGA - Transmitiendo bytes de rifol.zip desde la RAM del servidor...");
+    console.log("🚀 [${new Date().toISOString()}] DESCARGA - Transmitiendo bytes de rifol.zip desde la RAM del servidor...");
     
     const rutaArchivoFisico = path.join(__dirname, 'rifol.zip'); 
 
